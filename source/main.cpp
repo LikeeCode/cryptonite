6b4d9029a0bb203bdbcd5b3d575f55bcbc33cce6
RN <roma.edu.de@mail.ru>
2025-09-17 11:13:01 +0200

    add code for the minimal qml application

---
+++ source/main.cpp
@@ -0,0 +1,21 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    // Create the core application object
+    QGuiApplication app(argc, argv);
+
+    // Create the engine that will run the QML code
+    QQmlApplicationEngine engine;
+
+    // Define the URL to our main QML file.
+    // The "qrc:/" scheme means we are loading it from the compiled-in resources.
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+
+    // Load the QML file at the specified URL
+    engine.load(url);
+
+    // Start the Qt event loop and return the exit code when the app closes
+    return app.exec();
+}
