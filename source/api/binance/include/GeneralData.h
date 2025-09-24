3b7212b62745ce245236fff3471e9e745ee7594f
RN <roma.edu.de@mail.ru>
2025-09-25 01:05:37 +0200

    add structs for exchangeInfo

--- source/api/binance/include/GeneralData.h
+++ source/api/binance/include/GeneralData.h
@@ -23,4 +23,59 @@ namespace Binance::GeneralData
     {
         qint64 serverTime{};
     };
+
+    // Exchange information
+    // GET /api/v3/exchangeInfo
+    // Request
+    struct ExchangeInfoRequest{
+        QString symbol; // Optional
+        QList<QString> symbols; // Optional
+        AccountAndSymbolPermissions permissions{}; // Optional
+        bool showPermissionSets{}; // Optional
+        SymbolStatus status{}; // Optional
+    };
+
+    // Response
+    struct Symbol{
+        QString symbol;
+        SymbolStatus status{};
+        QString baseAsset;
+        int baseAssetPrecision{};
+        QString quoteAsset;
+        int quotePrecision{};
+        int quoteAssetPrecision{};
+        int baseCommissionPrecision{};
+        int quoteCommissionPrecision{};
+        QList<OrderType> orderTypes;
+        bool icebergAllowed{};
+        bool ocoAllowed{};
+        bool otoAllowed{};
+        bool quoteOrderQtyMarketAllowed{};
+        bool allowTrailingStop{};
+        bool cancelReplaceAllowed{};
+        bool amendAllowed{};
+        bool pegInstructionsAllowed{};
+        bool isSpotTradingAllowed{};
+        bool isMarginTradingAllowed{};
+        QList<FilterType> filters; // This uses the Filter variant from your Filters.h
+        QList<AccountAndSymbolPermissions> permissions;
+        QList<AccountAndSymbolPermissions> permissionSets;
+        QString defaultSelfTradePreventionMode;
+        QList<SelfTradePreventionMode> allowedSelfTradePreventionModes;
+    };
+
+    struct SOR
+    {
+        QString baseAsset;
+        QList<QString> symbols;
+    };
+    struct ExchangeInfo
+    {
+        QString timezone;
+        qint64 serverTime{};
+        QList<RateLimitType> rateLimits;
+        QList<FilterType> exchangeFilters;
+        QList<Symbol> symbols;
+        QList<Symbol> sors;
+    };
 }
