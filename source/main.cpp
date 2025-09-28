590c451aeaa0b06d6ca45336fd8d06e8fa939afd
RN <roma.edu.de@mail.ru>
2025-09-28 12:55:43 +0200

    bring back testing code to the main

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,9 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include "rest-api/BinanceAPI.h"
+#include "rest-api/GeneralDataParser.h"
+
 int main(int argc, char *argv[])
 {
     // Create the core application object
@@ -15,6 +18,30 @@ int main(int argc, char *argv[])
 
     // Load the QML file at the specified URL
     engine.load(url);
+
+    // Load Binance API
+    Binance::BinanceAPI *binanceApi = new Binance::BinanceAPI(&app, true); // true for test network
+    QObject::connect(binanceApi, &Binance::BinanceAPI::apiKeysFileError, []()
+                     { qDebug() << "API Keys file error!\n"; });
+    QObject::connect(binanceApi, &Binance::BinanceAPI::apiError, [](const QString &error)
+                     { qDebug() << "API error:\n"
+                                << error; });
+    QObject::connect(binanceApi, &Binance::BinanceAPI::pingResponse, [](const QJsonDocument &data)
+                     { qDebug() << "Ping response:\n"
+                                << data.toJson(QJsonDocument::Compact); });
+    QObject::connect(binanceApi, &Binance::BinanceAPI::timeResponse, [](const QJsonDocument &data)
+                     { qDebug() << "Time response:\n"
+                                << data.toJson(QJsonDocument::Compact); });
+    QObject::connect(binanceApi, &Binance::BinanceAPI::exchangeInfoResponse, [](const QJsonDocument &data)
+                     {
+                        qDebug() << "Exchange Info response:\n" << data.toJson(QJsonDocument::Compact);
+                        auto exchangeInfo = Binance::GeneralDataParser::parseExchangeInfo(data);
+                    });
+
+    // Example usage: Call some API methods
+    // binanceApi->ping();
+    // binanceApi->time();
+    binanceApi->exchangeInfo();
 
     // Start the Qt event loop and return the exit code when the app closes
     return app.exec();
